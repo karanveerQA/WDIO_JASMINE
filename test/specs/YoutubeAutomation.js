f8a85188cac82d643dc497f01db25dc1ffd9c5a2
karanveerQA <karanveer.singh@qualitestgroup.com>
2022-11-11 14:41:39 +0530

    new window test

--- test/specs/YoutubeAutomation.js
+++ test/specs/YoutubeAutomation.js
@@ -1,7 +1,7 @@
 import BasePage from "./../pages/BasePage";
 
 describe("youtube", () => {
-  it("should search the user input and show the results", async () => {
+  xit("should search the user input and check for channelName", async () => {
     await BasePage.openUrl("https://www.youtube.com/");
     const searchInputElement = await BasePage.searchInputField;
     const valueToSearch = "xqc";
@@ -16,5 +16,44 @@ describe("youtube", () => {
     const channelNameElement = await BasePage.channelName;
     const channelName = await channelNameElement.getText();
     expect(channelName).toEqual("xQc");
+  });
+
+  it("should open two tabs and search user input in both tabs and channelName in both tabs ", async () => {
+    await BasePage.openUrl("https://www.youtube.com/");
+    const searchInputElement = await BasePage.searchInputField;
+    let valueToSearch = "xqc";
+    await searchInputElement.addValue(valueToSearch);
+    const searchBtn = await BasePage.searchBtn;
+    await searchBtn.click();
+    //check for url change
+    let expectedUrl = "https://www.youtube.com/results?search_query=xqc";
+    await expect(browser).toHaveUrl(expectedUrl);
+
+    //check for channelName
+    const channelNameElement = await BasePage.channelName;
+    let channelName = await channelNameElement.getText();
+    expect(channelName).toEqual("xQc");
+
+    await browser.newWindow("https://www.youtube.com/", {
+      windowName: "Youtube window",
+      windowFeature: "width=300,height=150,resizable,scrollbars=yes,status=1",
+    });
+    valueToSearch = "ishowspeed";
+    await searchInputElement.addValue(valueToSearch);
+    await searchBtn.click();
+    //check for url change
+    expectedUrl = "https://www.youtube.com/results?search_query=ishowspeed";
+    await expect(browser).toHaveUrl(expectedUrl);
+
+    //check for channelName
+
+    channelName = await channelNameElement.getText();
+    expect(channelName).toEqual("IShowSpeed");
+
+    const handles = await browser.getWindowHandles();
+    //await browser.switchToWindow(handles[1]);
+    await browser.closeWindow();
+    await browser.switchToWindow(handles[0]);
+    await browser.closeWindow();
   });
 });
